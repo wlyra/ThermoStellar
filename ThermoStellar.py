e2126722c70e861e7c2845c58bee429bdc8be939
Wladimir Lyra <wladimir.lyra@gmail.com>
2022-05-14 16:05:02 -0600

    Remove comments on blank line, added more comments.

--- ThermoStellar.py
+++ ThermoStellar.py
@@ -51,36 +51,33 @@ def doubleExpCutoff(x,vmin,vmax):
     return d
 #------------------------------------------------
 
-# Grid 
+# Grid elements.
 nx = 512          # Resolution
 x0  = 1           # Grid left limit 
 xn  = 50          # Grid right limit
 
-# 
+# The code will calculate until either tmax or itmax is reached.
 
 tmax = 1000.      # Maximum integration time 
 itmax = 100000    # Maximum number of timesteps
 it_diagnos = 100  # Frequency in timesteps to print to screen
-#
-#  Times to plot the data
-#
+
+#  Times to plot the data.
+
 tout = np.array([1,3,10,30,100,300,1000])
-#
-#  Construct grid
-#
+
+#  Construct grid.
+
 ng = 3
 mx,l1,l2,i1,i2 = get_grid_indices(nx,ng)
 Lx = xn-x0
 dx = Lx/(nx-1)
 x = np.linspace(x0-3*dx,xn+3*dx,mx)
 
-# Initial Condition 
+# Initial Condition.
 
 N = 1/x * doubleExpCutoff(x, 1, 50)
 N = update_bounds(N)
-#    
-# Initialize Time
-t = 0     
 
 plt.plot(x[l1:l2],N[l1:l2],color='black',label=r'$\tau=0$')
 plt.xscale('log')
@@ -115,44 +112,40 @@ itout=0
 
 print("--- it --- t --- dt --- dt_advection --- dt_diffusion --- maxN --- minN")
 
+# Initialize Time and enter the time loop.
+
+t = 0
+
 for it in range(itmax):
-#
     for itsub in range(3):
         dNdt = alpha_ts[itsub]*dNdt
         ds   = alpha_ts[itsub]*ds
         ds=ds+1.
-#
+
         dNdt = dNdt + a[l1:l2]*der(N,x) + b*der2(N,x)
-#        
+        
         N[l1:l2] = N[l1:l2] + dt_beta_ts[itsub]*dNdt
-#        
+        
 #  Advance time 
-#
+
         t = t + dt_beta_ts[itsub]*ds
-#
+
 #  Apply boundary conditions for next stage
-#
+
         N = update_bounds(N)
 
-#
 #  Output to screen 
-#        
+        
     if (it % it_diagnos == 0): 
-        print(it,
-          t,
-          dt,
-          dt_advection,
-          dt_diffusion,
-          N[l1:l2].max(),N[l1:l2].min())
+        print(it,t,dt,dt_advection,dt_diffusion,N[l1:l2].max(),N[l1:l2].min())
 
-#
 #  Output to plot
-#
+
     dt_out = t-tout[itout]
     if ((dt_out > 0) and (dt_out < dt)):
         plt.plot(x[l1:l2],N[l1:l2],label=r'$\tau=$'+str(tout[itout]),linestyle='--')
         itout=itout+1        
-#
+
     if ((it == itmax) or t > tmax):
         print('End of simulation at t =',t)
         break
